2e42460c40d4695114deb6a0510c7be0a136e51f
jorislimonier <joris.limonier@hotmail.fr>
2021-05-17 16:15:02 +0200

    classes Fight and Judoka created

--- src/judoka.py
+++ src/judoka.py
@@ -1,7 +1,6 @@
-class Judoka():
-    def __init__(self, family_name, given_name, country):
+class Judoka:
+    def __init__(self, family_name, given_name, country, profile_url):
         self.family_name = family_name
         self.given_name = given_name
         self.country = country
-        
-
+        self.profile_url = profile_url
--- src/scraper.py
+++ src/scraper.py
@@ -1,29 +1,56 @@
-from src.judoka import Judoka; Judoka()
+from src.judoka import Judoka
+from src.fight import Fight
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains 
 import pytube
 
-# open browser
-def init_browser(path="chromedriver"):
-    browser = webdriver.Chrome(path)
-    browser.maximize_window()
-    return browser
 
-# download the video at a given url
-def download_video(url, location=None):
-    youtube = pytube.YouTube(url)
-    video = youtube.streams.first()
-    video.download(location)
+class Scraper:
+    def __init__(self, url):
+        self.url = "https://www.ijf.org/judoka?name=&nation=FRA&gender=both&category=sen"  # change later. Now only going through french athletes
 
+    def init_browser(url, path="chromedriver"):
+        """open browser"""
+        driver = webdriver.Chrome(path)
+        driver.maximize_window()
+        driver.get(url)
+        return driver
 
-url = "https://www.ijf.org/judoka?name=&nation=FRA&gender=both&category=sen"
-browser = init_browser()
-browser.get(url)
-judokas = browser.find_elements_by_class_name("judoka")
-judoka_card = judokas[0]
-judoka_info = judoka_card.find_element_by_class_name("judoka__info")
-family_name = judoka_info.find_element_by_class_name("family_name").text
-given_name = judoka_info.find_element_by_class_name("given_name").text
-country = judoka_info.find_element_by_class_name("country").text
-judoka = Judoka(family_name, given_name, country)
-browser.get(judoka_card.get_attribute("href") + '/videos')
+    def download_video(url, location=None):
+        """download the video at a given url"""
+        youtube = pytube.YouTube(url)
+        video = youtube.streams.first()
+        video.download(location)
+
+    def init_judoka(judoka_card):
+        """create instance of the Judoka class
+        with family_name, given_name and country"""
+        profile_url = judoka_card.get_attribute("href")
+        judoka_info = judoka_card.find_element_by_class_name("judoka__info")
+        family_name = judoka_info.find_element_by_class_name("family_name").text
+        given_name = judoka_info.find_element_by_class_name("given_name").text
+        country = judoka_info.find_element_by_class_name("country").text
+        return Judoka(profile_url, family_name, given_name, country)
+
+    def scrape_judokas(self, url):
+        driver = self.init_browser(url)
+        judoka_cards = driver.find_elements_by_class_name("judoka")
+        return [self.init_judoka(judoka_card) for judoka_card in judoka_cards[0:1]]
+
+    def scrape_judokas(self, judoka):
+        driver = self.init_browser(url)
+        driver.get(judoka.profile_url + "/contests")
+        driver.find_elements_by_class_name("opt")[1].click()  # click table view
+        table_rows = driver.find_elements_by_class_name("contest-table__contest")
+        for table_row in table_rows[0:1]:
+            table_row_white = table_row.find_element_by_class_name("judoka--white")
+            family_name = table_row_white.find_element_by_class_name(
+                "judoka__name"
+            ).text
+            country = table_row_white.find_element_by_class_name("country").text
+            # white =
+            # blue =
+            # competition =
+            # date =
+            # winner =
+            # category =
+            # comp_round =
